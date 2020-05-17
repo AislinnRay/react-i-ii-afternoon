1d481400f3cecc3896c25dddd26fc71c3d33c310
AislinnRay <aislinn.ray.m@gmail.com>
2020-05-17 09:43:09 -0600

    commit edits for clean-up

--- react-i-ii-afternoon/src/components/Citizen.js
+++ react-i-ii-afternoon/src/components/Citizen.js
@@ -3,27 +3,16 @@ import React from 'react';
 export default function Citizen(props){
     console.log('receiving props:' ,props)
     return <div>
-        <h1 className="ID">{props.citizen.id}/25</h1>
+        <p className="ID"><strong>{props.citizen.id}/25</strong></p>
         <h1 className="Name">{props.citizen.name.first} {props.citizen.name.last}</h1>
-        <span className="From">
-            <h3>From:  </h3>
-            <span>{props.citizen.city}, {props.citizen.country}</span>
-        </span>
-        <p className="Role"> 
-            <h3>Job Title:</h3>
-            <span>{props.citizen.title}</span>
-        </p>
-
-        <div className="Employer">
-            <h3>Employer:  </h3><span>{props.citizen.employer}</span>
-        </div>
-
+        <p className="From"><strong>From: </strong>{props.citizen.city}, {props.citizen.country}</p>
+        <p className="Role"><strong>Job Title: </strong>{props.citizen.title}</p>
+        <p className="Employer"><strong>Employer: </strong>{props.citizen.employer}</p>
         <p className="Movies">
-            <h3>Favorite Movies:</h3>
+            <strong>Favorite Movies:</strong>
             <p>1. {props.citizen.favoriteMovies[0]}</p>
             <p>2. {props.citizen.favoriteMovies[1]}</p>
             <p>3. {props.citizen.favoriteMovies[2]}</p>
         </p>
-
     </div>
 }
--- react-i-ii-afternoon/src/components/Next.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Next() {
-  return (
-    <div>
-      <h1>Next</h1>
-    </div>
-  );
-}
